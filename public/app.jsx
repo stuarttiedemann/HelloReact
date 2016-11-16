d3b3aa71d5b9116cbd5a437c523881f6e1435430
Stuart Tiedemann <tiedemannstuart@gmail.com>
2016-11-16 08:06:16 -0600

    added state and buttonclick functions

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,81 @@
-var Greeter = React.createClass({
+
+var GreeterMessage = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is a message from my first component</p>
+        <h1>Some h1</h1>
+        <p>Some paragraph</p>
       </div>
     );
   }
 });
 
+var GreeterForm = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <form>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is the default message'
+    };
+  },
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+
+    var name = nameRef.value;
+
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0){
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function() {
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+        <GreeterMessage/>
+
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+
+        <GreeterForm/>
+      </div>
+    );
+  }
+});
+
+var name = 'Stuart';
+var message = 'This is the alternative message';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={name} message={message}/>,
   document.getElementById('app')
 );
